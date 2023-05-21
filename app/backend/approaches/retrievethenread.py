305b0ccf685a2ba5022acae53a0dc15e1325d90c
bioerrorlog <bioerrorlog.contact@gmail.com>
2023-05-21 13:44:55 +0900

    use ChatCompletion instead of Completion in rtr approach

--- app/backend/approaches/retrievethenread.py
+++ app/backend/approaches/retrievethenread.py
@@ -9,17 +9,16 @@ from text import nonewlines
 # (answer) with that prompt.
 class RetrieveThenReadApproach(Approach):
 
-    template = \
+    prompt_prefix = \
 "You are an intelligent assistant helping Contoso Inc employees with their healthcare plan questions and employee handbook questions. " + \
 "Use 'you' to refer to the individual asking the questions even if they ask with 'I'. " + \
 "Answer the following question using only the data provided in the sources below. " + \
 "For tabular information return it as an html table. Do not return markdown format. "  + \
 "Each source has a name followed by colon and the actual information, always include the source name for each fact you use in the response. " + \
-"If you cannot answer using the sources below, say you don't know. " + \
-"""
+"If you cannot answer using the sources below, say you don't know. "
 
-###
-Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
+    user_oneshot = \
+"""Question: 'What is the deductible for the employee plan for a visit to Overlake in Bellevue?'
 
 Sources:
 info1.txt: deductibles depend on whether you are in-network or out-of-network. In-network deductibles are $500 for employee and $1000 for family. Out-of-network deductibles are $1000 for employee and $2000 for family.
@@ -28,10 +27,11 @@ info3.pdf: Overlake is the name of the area that includes a park and ride near B
 info4.pdf: In-network institutions include Overlake, Swedish and others in the region
 
 Answer:
-In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf].
+"""
+    answer_oneshot = "In-network deductibles are $500 for employee and $1000 for family [info1.txt] and Overlake is in-network for the employee plan [info2.pdf][info4.pdf]."
 
-###
-Question: '{q}'?
+    template = \
+"""Question: '{q}'?
 
 Sources:
 {retrieved}
@@ -69,12 +69,18 @@ Answer:
         content = "\n".join(results)
 
         prompt = (overrides.get("prompt_template") or self.template).format(q=q, retrieved=content)
-        completion = openai.Completion.create(
-            engine=self.openai_deployment, 
-            prompt=prompt, 
-            temperature=overrides.get("temperature") or 0.3, 
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": self.prompt_prefix},
+                {"role": "user", "content": self.user_oneshot},
+                {"role": "assistant", "content": self.answer_oneshot},
+                {"role": "user", "content": prompt},
+            ],
+            temperature=overrides.get("temperature") or 0,
             max_tokens=1024, 
             n=1, 
-            stop=["\n"])
+            stop=["\n"]
+        )
 
-        return {"data_points": results, "answer": completion.choices[0].text, "thoughts": f"Question:<br>{q}<br><br>Prompt:<br>" + prompt.replace('\n', '<br>')}
+        return {"data_points": results, "answer": completion.choices[0].message.content, "thoughts": f"Question:<br>{q}<br><br>Prompt:<br>" + prompt.replace('\n', '<br>')}
